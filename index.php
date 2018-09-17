a0e528e3919b69c48604605ecfe97c1f53407aac
LocNguyen <kten1112@gmail.com>
2018-09-17 23:41:33 +0700

    Add parameter (true, false) to choose return or echo data. Complete

--- index.php
+++ index.php
@@ -7,40 +7,29 @@ include 'database/Database.php';
 class ShowTreeMenu extends Database
 {
 
-    public function trueStatus($categories, $parent_id = 0)
+    public function trueStatus($categories, $parent_id = 0, $bool = true)
     {
         $menu_html = '<ul>';
         foreach ($this->categories as $value) {
             if ($value['parent_id'] == $parent_id) {
-                // echo $value['name'];
                 $menu_html .= '<li>' . $value['name'];
-                $menu_html .= $this->trueStatus($categories, $value['menu_id']) . '</li>';
+                $menu_html .= $this->trueStatus($categories, $value['menu_id'], true) . '</li>';
             }
         }
         $menu_html .= '</ul>';
-        return $menu_html;
-    }
-
-    public function checkStatus($bool = true)
-    {
-        if ($bool != true) 
+        
+        if($bool)
         {
-            echo($this->menu_html);
-        } 
+            return $menu_html;
+        }
         else
         {
-            return $this->trueStatus();
-            var_dump($this->menu_html);
+            echo($menu_html);
         }
     }
 }
 
 $treeMenu = new ShowTreeMenu();
-// $treeMenu->trueStatus($categories, 0);
-$treeMenu->checkStatus(true);
 
-echo($treeMenu->trueStatus($categories,0));
-
-// $treeMenu->showTreeItem($categories, 0, true);
-// $treeMenu->orderedMenu($categories,0);
+$treeMenu->trueStatus($categories, 0, false);
 ?>
